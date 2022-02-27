e39648bfaaeab88b5264b465087a9e5dce8cc8d2
dinianjelina <dinianjelina12@gmail.com>
2022-02-27 00:10:49 -0800

    Update file App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//           <br />
+//           oke sudah ditambah
+//         </p>
+//         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+import logo from './dinianjelina.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Eko ganteng</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="dinianjelina" />
+        <p>
+          Dini Anjelina
+          <br />
+          Pengusaha || Web Programmer
+        </p>
+        <a className="App-link" href="https://www.instagram.com/dinianjelina_/" target="_blank" rel="noopener noreferrer">
+          dinianjelina
+        </a>
+      </header>
     </div>
   );
 }
